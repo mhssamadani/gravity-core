b1bf689e3d2012101038f1326bcdecac962eb523
Mostafa Hosseini <aseyedmostafaho@gmail.com>
2021-07-16 05:00:59 +0000

    Bugfix parse priv key

--- common/account/nebula.go
+++ common/account/nebula.go
@@ -21,7 +21,7 @@ func StringToNebulaId(address string, chainType ChainType) (NebulaId, error) {
 	var nebula NebulaId
 
 	switch chainType {
-	case Ethereum, Binance, Heco, Fantom, Avax, Polygon, Ergo:
+	case Ethereum, Binance, Heco, Fantom, Avax, Polygon:
 		nebulaBytes, err := hexutil.Decode(address)
 		if err != nil {
 			return NebulaId{}, err
@@ -30,7 +30,7 @@ func StringToNebulaId(address string, chainType ChainType) (NebulaId, error) {
 	case Waves:
 		nebulaBytes := crypto.MustBytesFromBase58(address)
 		nebula = BytesToNebulaId(nebulaBytes)
-	case Solana:
+	case Solana, Ergo:
 		nebulaBytes := base58.Decode(address)
 		nebula = BytesToNebulaId(nebulaBytes)
 		zap.L().Sugar().Debug("NebulaId: ", nebula)
@@ -53,9 +53,9 @@ func BytesToNebulaId(value []byte) NebulaId {
 func (id NebulaId) ToString(chainType ChainType) string {
 	nebula := id.ToBytes(chainType)
 	switch chainType {
-	case Ethereum, Binance, Heco, Fantom, Avax, Polygon, Ergo:
+	case Ethereum, Binance, Heco, Fantom, Avax, Polygon:
 		return hexutil.Encode(nebula[:])
-	case Waves:
+	case Waves, Ergo:
 		return base58.Encode(nebula[:])
 	case Solana:
 		return base58.Encode(nebula[:])
